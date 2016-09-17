3a7e7f079ef7eb437c1fecb117a1ff051c9d20b8
Barada <telnowtema@mail.ru>
2016-09-17 15:53:20 +0300

    nachalo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deopt2::Application.config.secret_key_base = '8882063d8ba0d2133d9f4c0736d1842f21bd874b1f3b053dbe7861e27424465561abff6d7c8584c471c90625e87324804ef78cfc3e3410f08124ef93e12420e6'
